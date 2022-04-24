0dc23797bd98b0de177dd822f6f54710c21f769d
VONETTA <thevonetteway@gmail.com>
2022-04-24 17:11:44 +0300

    adding functions,save credentials,delete credentialsand class method find aacount and checking an existing account

--- credentials.py
+++ credentials.py
@@ -16,6 +16,35 @@ class credential:
       """
       Function to save users account credentials
       """
-      
+       # self.new_credential.save_credential()
       credential.credentials_list.append(self)
       
+    def del_credentials(self):
+        """
+        Function to delete a users account credentials
+        """
+        
+        credential.credential_list.remove(self)
+        
+        
+    @classmethod
+    def find_by_account(cls,account_details):
+        
+        for credential in cls.credentials_list: 
+            if credential.account_details==account_details:
+                return credential
+            
+    @classmethod
+    def account_exists(cls,account_details):
+        for credential in cls.credentials_list:
+            if credential.account_details== account_details:
+                return True
+        return False
+    
+    # display Credential
+    @classmethod
+    def display_credential(cls):
+        return cls.credentials_list
+        
+        
+    
