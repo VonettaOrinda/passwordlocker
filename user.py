20fea94f7be2bec8eba7c02daae0f3036e2b2ea6
VONETTA <thevonetteway@gmail.com>
2022-04-24 17:26:08 +0300

    add class user and define password and username

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    user_list=[]
+    def __init__(self,username,userpassword):
+        self.username = username
+        self.userpassword = userpassword
+       
+        # saving user
+    def save_user(self):
+        
+        User.user_list.append(self)
+        
+    def  login(self):
+        if User in User.user_list:
+            print(User)
+            return User
