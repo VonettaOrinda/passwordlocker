1a8759c3e102b8d5e7b7ff823b49412e8a708378
VONETTA <thevonetteway@gmail.com>
2022-04-24 16:23:33 +0300

    add the instance variables,account details,username and password

---
+++ credentials.py
@@ -0,0 +1,13 @@
+class credential:
+    """
+    class that generates new instances of credentials.
+    """
+    credential_list=[] #Empty credential list
+    
+    def__init__(self,account_details,username,password):
+        
+        self.account_details= account_details
+        self.username= username
+        self.password= password
+        
+    credential_list=[]
