d3542d18f56a04c6ef20faeb704060cc6802b108
VONETTA <thevonetteway@gmail.com>
2022-04-24 19:13:14 +0300

    adding string and random to generate passwords using a string of letters,digits and special characters

---
+++ password.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+def generate_Password (stringLength=8) :
+        
+        """Generate a random password string of letters and digits and special characters"""
+        
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
+ 
+    
+    
+    
+    
+    
+    
